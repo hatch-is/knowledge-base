ad8d51d7c8d6d4966e3866760dcd9a819539060c
vanya andreychuk <vandreychyk@gmail.com>
2016-09-23 20:05:51 +0300

    - fix build scrypt

--- build.sh
+++ build.sh
@@ -15,8 +15,9 @@ GIT=git@github.com:hatch-is/knowledge-base.git
 
 rm -rf ./_build/.cache/
 git clone $GIT ./_build/.cache/knowledge-base
-cd ./_build/.cache/
+cd ./_build/.cache/knowledge-base
 git checkout $1
+cd ../
 docker build -t $ECR_PATH:$1 ../
 docker push $ECR_PATH:$1
 
